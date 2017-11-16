0bd896566281faa3c0c350c30189529c9a235488
Cadir <andrei-dragos.cadir@accenture.com>
2017-11-16 11:48:58 +0200

    Module 10 - Testing commit

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,20 @@
-let express = require('express')
-let path = require('path')
-let open = require('open')
+import express from 'express'
+import path from 'path'
+import open from 'open'
+import webpack from 'webpack'
+import config from '../webpack.config.dev'
+
+/* eslint-disable no-console*/
 
 let port = 3000
 let app = express()
+let compiler = webpack(config)
+
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}))
+
 app.get('/', function(req,res) {
     res.sendFile(path.join(__dirname,'../src/index.html'))
 })
